8cff6921009902d066b8ae1754c29d3461792164
Ilia Proskurin <pilailiapro@gmail.com>
2025-06-09 11:14:31 +0300

    add time

--- src/components.jsx
+++ src/components.jsx
@@ -34,10 +34,63 @@ export const OnAddButtonClick = ({
   );
 };
 
+const getDateNow = () => {
+  let date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  if (minutes.toString().length < 2) {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":0" +
+      minutes +
+      ":" +
+      seconds
+    );
+  } else if (seconds.toString().length < 2) {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":0" +
+      seconds
+    );
+  } else {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds
+    );
+  }
+};
+
 export const AddList = ({ list }) => {
   return list.map((item) => (
     <li key={item.id} className="listItem">
-      {item.value}
+      {`${item.value} ${getDateNow()}`}
     </li>
   ));
 };
