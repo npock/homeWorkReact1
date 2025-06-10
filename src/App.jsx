8a423a8ff0935abc46986a56faa97202d842c9f0
Ilia Proskurin <pilailiapro@gmail.com>
2025-06-10 06:39:43 +0300

    correcting isValuevalid components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,73 @@
 import "./app.modules.css";
 import { useState } from "react";
-import * as components from "./components";
+import * as components from "./components.buttons";
+
+const getDateNow = () => {
+  let date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  if (minutes.toString().length < 2) {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":0" +
+      minutes +
+      ":" +
+      seconds
+    );
+  } else if (seconds.toString().length < 2) {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":0" +
+      seconds
+    );
+  } else {
+    return (
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds
+    );
+  }
+};
+
+const AddList = ({ list }) => {
+  return list.map((item) => (
+    <li key={item.id} className="listItem">
+      {`${item.value} ${getDateNow()}`}
+    </li>
+  ));
+};
 
 export const App = () => {
   const [value, setValue] = useState("");
   const [list, setList] = useState([]);
   const [error, setError] = useState("");
-  let isValueVaild;
-  value.length < 3 ? (isValueVaild = false) : (isValueVaild = true);
+  let isValueVaild = value.length < 3;
 
   return (
     <div className="app">
@@ -25,7 +85,7 @@ export const App = () => {
           setError={setError}
         />
         <components.OnAddButtonClick
-          isValueVaild={isValueVaild}
+          isValueVaild={!isValueVaild}
           setList={setList}
           value={value}
           setValue={setValue}
@@ -38,7 +98,7 @@ export const App = () => {
           <p className="noMarginText">Нет добавленных элементов</p>
         )}
         <ul className="list">
-          <components.AddList list={list} />
+          <AddList list={list} />
         </ul>
       </div>
     </div>
